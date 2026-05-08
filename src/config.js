0139c6d8816c1aca6ccb23dbd71c9d3e304a7ef1
Ahmed UA. <ahmed_umer_aziz@live.com>
2026-05-08 14:38:27 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // ─── CONFIG ───────────────────────────────────────────────────────────────────
 export const SHEET_ID        = "1_iXcsPI8C1g0UQaAcacbKjsHq9AWI3IRIsCbX2E87qk";
-export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz-h1jphatR49vp7AFO6gUJphpl1d-Si1iVEhJaLNiP8tUgbEy9ACEH-DHq4UtE4l0e/exec";
+export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzf5jyvgzh0MwBjpUe1ECF2XIlJGL72WrNuBEGVGrbodXg35UWn_cg9jFW4ZKdZgu5W/exec";
 
 export const SHEET_URLS = {
   items:      `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv&gid=0`,
